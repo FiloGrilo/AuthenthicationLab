ca8916ea4066c35753afeb4f4107fea9aa26479e
Tomás Estácio <97453864+tomasestacio@users.noreply.github.com>
2022-10-30 14:32:09 +0100

    Update PasswordService.java

--- src/main/java/server/PasswordService.java
+++ src/main/java/server/PasswordService.java
@@ -5,11 +5,19 @@ public class PasswordService implements IPasswordService {
 
     @Override
     public boolean verifyUser(String username, String password) {
+        //go to database
+        //search for username
+        //use same hash function on password passed and check if its equal to saved password associated with that username
+        //if yes, return true
+        //if not, return false
         return false;
     }
 
     @Override
     public void saveUser(String username, String password) {
-
+        //go to database
+        //check if there is no same username
+        //if it is, return system.out.println("Username already exists, please choose another one.");
+        //if not, create username and store in database the username, the salt used and the hash function of password+salt
     }
 }
